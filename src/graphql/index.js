cdcb2e5d4d44bd6a68ee6713e87321d3579a27eb
LittleBigPants <juanmanuel_garcia98@hotmail.com>
2023-12-13 11:00:18 -0300

    scalar-types

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -12,6 +12,11 @@ const typeDefs = `
   type Query {
     hello: String
     getPerson(name: String!, age: Int!): String!
+    getInt(age: Int!): Int
+    getFloat(price: Float!): Float
+    getString: String
+    getBoolean: Boolean
+    getID:ID
   }
 `
 
@@ -28,7 +33,12 @@ const resolvers = {
     hello: () => 'Hola mundillo',
     // getPerson: () => [1, 2, 3]
     // getPerson: () => 12
-    getPerson: (_, args) => `hello, my name is ${args.name}, i am ${args.age} years old`
+    getPerson: (_, args) => `hello, my name is ${args.name}, i am ${args.age} years old`,
+    getInt: (_, args) => args.age,
+    getFloat: (_, args) => args.price,
+    getString: () => 'palabrea',
+    getBoolean: () => true,
+    getID: () => '123456'
 
   }
 }
