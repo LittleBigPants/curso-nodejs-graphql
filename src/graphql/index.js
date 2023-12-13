0fccfbad4d31f325b7f4480499471b4438f3f329
LittleBigPants <juanmanuel_garcia98@hotmail.com>
2023-12-13 11:38:40 -0300

    objects-types-&-fields

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -6,20 +6,34 @@ const { ApolloServer } = require('@apollo/server')
 const { ApolloServerPluginLandingPageLocalDefault } = require('@apollo/server/plugin/landingPage/default')
 // Middleware de Express también en @apollo/server
 const { expressMiddleware } = require('@apollo/server/express4')
+const { date } = require('joi')
 
 //los parametros de la query son opcionales. Si queremos que sean obligatorios sus types deben tener un signo !:
+
+// el signo ! me obliga a cumplir con lo el tipo de query que pido
 const typeDefs = `
   type Query {
-    hello: String
+    hello: String!
     getPerson(name: String!, age: Int!): String!
     getInt(age: Int!): Int
     getFloat(price: Float!): Float
     getString: String
     getBoolean: Boolean
     getID:ID
+    getNumbers(numbers: [Int!]!): [Int]
+    getProduct: Product
+  }
+
+  type Product {
+    id: ID!
+    name: String!
+    price: Float!
+    description: String!
+    image: String!
+    createdAt: String!
   }
 `
-
+// Scalar types
 // Get = Query
 // POST, PUT , DELETE = Mutations
 // POST = 201
@@ -28,9 +42,13 @@ const typeDefs = `
 // PUT = Update
 // DELETE = remove
 
+// List
+// [String]
+// [Int]
+
 const resolvers = {
   Query: {
-    hello: () => 'Hola mundillo',
+    hello: () => 'buenas muchachos',
     // getPerson: () => [1, 2, 3]
     // getPerson: () => 12
     getPerson: (_, args) => `hello, my name is ${args.name}, i am ${args.age} years old`,
@@ -38,7 +56,18 @@ const resolvers = {
     getFloat: (_, args) => args.price,
     getString: () => 'palabrea',
     getBoolean: () => true,
-    getID: () => '123456'
+    getID: () => '123456',
+    getNumbers: (_, args) => args.numbers,
+    getProduct: () => {
+      return {
+        id: '1212',
+        name: 'product 1',
+        price: 100.12,
+        description: 'bla bla bla bla',
+        image: 'http://image.asas',
+        createdAt: new Date().toISOString()
+      }
+    }
 
   }
 }
