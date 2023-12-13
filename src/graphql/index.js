cb0bf35682a16b9c34b8bf6169e001122c61108e
LittleBigPants <juanmanuel_garcia98@hotmail.com>
2023-12-13 12:17:01 -0300

    archivos-graphql

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,38 +1,43 @@
 // /graphql/index.js
 
 // Dependencia que crea un servidor
-const { ApolloServer } = require('@apollo/server')
+const { ApolloServer } = require('@apollo/server');
 // Playground incluido en @apollo/server
-const { ApolloServerPluginLandingPageLocalDefault } = require('@apollo/server/plugin/landingPage/default')
+const {
+  ApolloServerPluginLandingPageLocalDefault,
+} = require('@apollo/server/plugin/landingPage/default');
 // Middleware de Express también en @apollo/server
-const { expressMiddleware } = require('@apollo/server/express4')
-const { date } = require('joi')
+const { expressMiddleware } = require('@apollo/server/express4');
+
+const { loadFiles} = require('@graphql-tools/load-files');
+
+
 
 //los parametros de la query son opcionales. Si queremos que sean obligatorios sus types deben tener un signo !:
 
 // el signo ! me obliga a cumplir con lo el tipo de query que pido
-const typeDefs = `
-  type Query {
-    hello: String!
-    getPerson(name: String!, age: Int!): String!
-    getInt(age: Int!): Int
-    getFloat(price: Float!): Float
-    getString: String
-    getBoolean: Boolean
-    getID:ID
-    getNumbers(numbers: [Int!]!): [Int]
-    getProduct: Product
-  }
+// const typeDefs = `
+//   type Query {
+//     hello: String!
+//     getPerson(name: String!, age: Int!): String!
+//     getInt(age: Int!): Int
+//     getFloat(price: Float!): Float
+//     getString: String
+//     getBoolean: Boolean
+//     getID:ID
+//     getNumbers(numbers: [Int!]!): [Int]
+//     getProduct: Product
+//   }
 
-  type Product {
-    id: ID!
-    name: String!
-    price: Float!
-    description: String!
-    image: String!
-    createdAt: String!
-  }
-`
+//   type Product {
+//     id: ID!
+//     name: String!
+//     price: Float!
+//     description: String!
+//     image: String!
+//     createdAt: String!
+//   }
+// `;
 // Scalar types
 // Get = Query
 // POST, PUT , DELETE = Mutations
@@ -51,7 +56,8 @@ const resolvers = {
     hello: () => 'buenas muchachos',
     // getPerson: () => [1, 2, 3]
     // getPerson: () => 12
-    getPerson: (_, args) => `hello, my name is ${args.name}, i am ${args.age} years old`,
+    getPerson: (_, args) =>
+      `hello, my name is ${args.name}, i am ${args.age} years old`,
     getInt: (_, args) => args.age,
     getFloat: (_, args) => args.price,
     getString: () => 'palabrea',
@@ -65,31 +71,30 @@ const resolvers = {
         price: 100.12,
         description: 'bla bla bla bla',
         image: 'http://image.asas',
-        createdAt: new Date().toISOString()
-      }
-    }
-
-  }
-}
+        createdAt: new Date().toISOString(),
+      };
+    },
+  },
+};
 
 const useGraphQL = async (app) => {
   const server = new ApolloServer({
-    typeDefs,
+    typeDefs: await loadFiles('./src/**/*.graphql'), //esto para que tome todas los archivos .graphql
     resolvers,
     playground: true,
-    plugins: [
-      ApolloServerPluginLandingPageLocalDefault
-    ]
-  })
+    plugins: [ApolloServerPluginLandingPageLocalDefault],
+  });
 
-  await server.start()
+  await server.start();
 
-// Uso del middleware en Express
-  app.use(expressMiddleware(server,{
-    context: async ({req}) => ({
-      token: req.headers.token
+  // Uso del middleware en Express
+  app.use(
+    expressMiddleware(server, {
+      context: async ({ req }) => ({
+        token: req.headers.token,
+      }),
     })
-  }))
-}
+  );
+};
 
-module.exports = useGraphQL
+module.exports = useGraphQL;
