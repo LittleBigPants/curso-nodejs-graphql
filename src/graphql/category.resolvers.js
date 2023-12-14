1900476927c641e892048ecdcbab1445c4a94215
LittleBigPants <juanmanuel_garcia98@hotmail.com>
2023-12-14 15:45:21 -0300

    autorizacion

--- src/graphql/category.resolvers.js
+++ src/graphql/category.resolvers.js
@@ -1,8 +1,12 @@
 const CategoryService = require('./../services/category.service');
 const service = new CategoryService();
+const boom = require('@hapi/boom');
 
-const addCategory = (_, { dto, context }) => {
-  context.au
+const addCategory = async(_, { dto }, context) => {
+  const { user } = await context.authenticate('jwt', {session: false});
+  if (!user) {
+    throw boom.unauthorized("no esta autorizadfo")
+  }
   return service.create(dto)
 }
 
